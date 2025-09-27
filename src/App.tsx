ca6ef65b91c92efb10748615690fac9779a81181
kimu8120 <kimura0223sota@gmail.com>
2025-09-27 23:03:44 +0900

    ブログのページ遷移の完成

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home' // 先ほど作ったHome.tsxをインポート
 import './App.css'
+import Blog from './pages/Blog'
 
 function App() {
   return (
-    <Routes>
-      {/* URLが「/」の時に、Homeコンポーネントを表示するという設定 */}
-      <Route path="/" element={<Home />} />
-    </Routes>
-  )
+    <>
+      <Routes>
+        {/* URLが「/」の時に、Homeコンポーネントを表示するという設定 */}
+        <Route path="/" element={<Home />} />
+        <Route path="/blog" element={<Blog />} />
+      </Routes>
+    
+        <div>
+          <button>
+            <a href="/blog">ブログへ</a>
+          </button>
+        </div>
+    </>
+  );
 }
+
 
 export default App;
---
+++ src/pages/Blog.tsx
@@ -0,0 +1,10 @@
+function Blog() {
+    return ( 
+    <div>
+        <h1>Blog</h1>
+        <p>こんにちは！</p>
+        <p>ブログページです</p>
+    </div>
+    );
+}
+export default Blog;
