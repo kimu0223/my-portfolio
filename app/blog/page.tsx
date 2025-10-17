3241b605e603297f3e3a6c4d7b0baff376decda0
kimu8120 <kimura0223sota@gmail.com>
2025-10-18 06:06:48 +0900

    feat: microCMSを導入しブログ機能を追加

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import Link from "next/link";
+import { getList } from "../../lib/microcms";
 
-export default function Blog() {
+export default async function StaticPage() {
+  const { contents } = await getList();
+
+  // データの取得ができない・記事が0件の場合はエラー表示
+  if (!contents || contents.length === 0) {
+    return <h1>No contents</h1>;
+  }
+
   return (
     <div>
       <h1>Blog</h1>
-      <p>こんにちは！</p>
-      <p>ブログページです</p>
+      <ul>
+        {contents.map((post) => {
+          return (
+            <li key={post.id}>
+              <Link href={`/blog/${post.id}`}>{post.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
---
+++ lib/microcms.ts
@@ -0,0 +1,56 @@
+import { createClient } from "microcms-js-sdk";
+import type {
+  MicroCMSQueries,
+  MicroCMSImage,
+  MicroCMSDate,
+} from "microcms-js-sdk";
+
+// ブログの型定義
+export type Blog = {
+  id: string;
+  title: string;
+  content: string;
+  eyecatch?: MicroCMSImage;
+} & MicroCMSDate;
+
+if (!process.env.MICROCMS_SERVICE_DOMAIN) {
+  throw new Error("MICROCMS_SERVICE_DOMAIN is required");
+}
+
+if (!process.env.MICROCMS_API_KEY) {
+  throw new Error("MICROCMS_API_KEY is required");
+}
+
+if (!process.env.MICROCMS_API_ENDPOINT) {
+    throw new Error("MICROCMS_API_ENDPOINT is required");
+}
+
+// APIキーとサービスドメイン/エンドポイントを設定
+export const client = createClient({
+  serviceDomain: process.env.MICROCMS_SERVICE_DOMAIN,
+  apiKey: process.env.MICROCMS_API_KEY,
+});
+
+const endpoint = process.env.MICROCMS_API_ENDPOINT;
+
+// ブログ一覧を取得
+export const getList = async (queries?: MicroCMSQueries) => {
+  const listData = await client.getList<Blog>({
+    endpoint: endpoint,
+    queries,
+  });
+  return listData;
+};
+
+// ブログの詳細を取得
+export const getDetail = async (
+  contentId: string,
+  queries?: MicroCMSQueries
+) => {
+  const detailData = await client.get<Blog>({
+    endpoint: endpoint,
+    contentId,
+    queries,
+  });
+  return detailData;
+};
